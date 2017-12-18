9d51bbf127984ec434f49b9ef857a6ce57a2290f
BEBAN <babantoto@gmail.com>
2017-12-18 14:43:53 +0800

    Minor changes -bevan

---
+++ src/tagabukid/hrmis/pmis/models/PerformanceCategoryController.groovy
@@ -0,0 +1,15 @@
+import com.rameses.rcp.common.*;
+import com.rameses.rcp.annotations.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+        
+public class PerformanceCategoryController extends CRUDController
+{
+    String serviceName = 'TagabukidPerformanceCategoryService'
+    String entityName  = 'performancecategory'
+    String prefixId    = 'PC'
+            
+    def selectedItem;
+        
+}
+
